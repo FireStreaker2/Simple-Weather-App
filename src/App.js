77a2e5f7e8e8539038233d47833f98580505554b
FireStreaker2 <103970465+FireStreaker2@users.noreply.github.com>
2023-05-18 18:53:35 -0700

    data fetch rework

--- src/App.js
+++ src/App.js
@@ -1,85 +1,56 @@
+import React, { useState } from "react";
 import logo from './sun.png';
 import './App.css';
-import * as dotenv from "dotenv";
-dotenv.config()
 
-const showAlert = () => {
-  var url = window.location.href;
-  var queryString = `${window.location.protocol}//${window.location.hostname}/?search=`;
-  var query = url.replace(queryString, "");
-  
-  const body = document.getElementById("canvas");
-  const popup = document.createElement("div");
-  var locationText = document.createElement("h1");
-  locationText.innerHTML = query
-    
-  popup.style.width = "50%";
-  popup.style.height = "50%";
-    
-  body.appendChild(popup);
-  popup.appendChild(locationText);
+const fetchData = (location, setData) => {
+  const key = process.env.REACT_APP_KEY;
+  var api = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`;
 
-  const key = process.env.KEY;
-  const api = "https://api.weatherapi.com/v1/current.json?key=" + key + "&q=" + query;
-
-  // const [data, setData] = useState(null);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await fetch(api);
-  //     const jsonData = await response.json();
-  //     setData(jsonData);
-  //   };
-
-  //   fetchData();
-  // }, []);
-
-  //retrieve data
   fetch(api)
   .then((response) => (response.json()))
   .then((data) => {
-    console.log(data);
-    
-    // store data
-    var name = data.location.name;
-    var region = data.location.region;
-    var celcius = data.current.temp_c;
-    var farenheit = data.current.temp_f;
-    
-    var wind_mph = data.current.wind_mph;
-    var wind_kph = data.current.wind_kph;
-    
-    var humidity = data.current.humidity;
-    
-    // make popup work
-    nameText = document.createElement("p");
-    regionText = document.createElement("p");
-    celciusText = document.createElement("p");
-    farenheitText = document.createElement("p");
-    windMPHText = document.createElement("p");
-    windKPHText = document.createElement("p");
-    humidityText = document.createElement("p");
-    
-    nameText.innerHTML = name;
-    regionText.innerHTML = region;
-    celciusText.innerHTML = celcius;
-    farenheitText.innerHTML = farenheit;
-    windMPHText.innerHTML = wind_mph;
-    windKPHText.innerHTML = wind_kph;
-    humidityText.innerHTML = humidity;
-    
-    popup.appendChild(nameText);
-    popup.appendChild(regionText);
-    popup.appendChild(celciusText);
-    popup.appendChild(farenheitText);
-    popup.appendChild(windMPHText);
-    popup.appendChild(windKPHText);
-    popup.appendChild(humidityText);
-  
-});
+    var stringData = JSON.stringify(data);
+    setData(stringData);
+  })
+  .catch(error => {
+    console.error(error);
+    alert(error);
+  });
+}
+
+function WeatherResult({ data, onClose }) {
+  return (
+    <div className="popup">
+      <div className="popup-content">
+        <h2>Your Weather Result!</h2>
+        <p>{data}</p>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
 }
 
 function App() {
+	
+	const [inputValue, setInputValue] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
+	
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Form submitted with value:", inputValue);
+    fetchData(inputValue, setWeatherData);
+    setShowPopup(true);
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+  
   return (
     <div className="App" id="canvas">
       <header className="App-header">
@@ -87,20 +58,21 @@ function App() {
 
         <p>Welcome to the Simple Weather App! Get started below.</p>
 
-        <form action="" class="search-bar">
-          <input type="search" placeholder="Enter Coordinates Here." name="search" pattern=".*\S.*" required/>
-          <button class="search-btn" type="submit" onClick={showAlert}>
+        <form onSubmit={handleSubmit} class="search-bar">
+          <input type="search" placeholder="Enter Coordinates Here." name="search" pattern=".*\S.*" required onChange={handleChange} />
+          <button class="search-btn" type="submit">
             <span>Search</span>
           </button>
         </form>
-		
-		<footer>
-			<a href="https://github.com/FireStreaker2/Simple-Weather-App" class="link">GitHub |</a>
-			<a href="https://github.com/FireStreaker2/Simple-Weather-App/issues" class="link"> Support |</a>
-			<a href="https://github.com/FireStreaker2/Simple-Weather-App/blob/main/LICENSE" class="link"> License</a>
-		</footer>
 
-    <script src="./search.js"></script>
+        {weatherData && showPopup && <WeatherResult data={weatherData} onClose={handleClosePopup} />}
+		
+        <footer>
+          <a href="https://github.com/FireStreaker2/Simple-Weather-App" class="link">GitHub |</a>
+          <a href="https://github.com/FireStreaker2/Simple-Weather-App/issues" class="link"> Support |</a>
+          <a href="https://github.com/FireStreaker2/Simple-Weather-App/blob/main/LICENSE" class="link"> License</a>
+        </footer>
+
       </header>
     </div>
   );
